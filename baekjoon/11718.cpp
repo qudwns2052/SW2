9664d152f95feb852f032965b3ddff5b7d60b7fc
최병준 <qudwns2052@gmail.com>
2019-03-08 14:28:07 +0900

    190308

---
+++ baekjoon/11718.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string str;
+	int cnt = 0;
+	while(1)
+	{
+		cin.clear();
+		getline(cin, str);
+		cout << str << endl;
+		cnt++;
+		if (cnt >= 100)
+			break;
+	}
+	return 0;
+}
