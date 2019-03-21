a68b1880670951cc66f0d3288d81f2d0f7ea57d9
최병준 <qudwns2052@gmail.com>
2019-03-21 11:19:31 +0900

    190321

--- 190319/1.c
+++ 190319/1.c
@@ -3,13 +3,17 @@
 
 #define LEN 10
 
+
 int main()
 {
 	int arr[20];
-	int result[20] = { 0 };
+	int result[20] = { 0 };  
+	int temp[50] = { 0 };
+	int mresult[50] = { 0 };
+	int tvalue1, tvalue2;
 	int i, j;
 	int idx1, idx2;
-	int fidx, sidx, cidx;
+	int fidx, sidx, cidx, ridx;
 	int cnt;
 
 
@@ -29,7 +33,6 @@ int main()
 
 	}
 
-	printf("\n\n두 다항식을 더합니다\n\n");
 
 	fidx = 0;
 	sidx = 0;
@@ -91,17 +94,98 @@ int main()
 		
 	}
 
+	fidx = 0;
+	cidx = 0;
+
+
+	for (i = 0; i < 5; i++)
+	{ 
+		sidx = 0;
+		
+		idx1 = (fidx * 2) + 1;
+
+		for (j = 0; j < 5; j++)
+		{
+			idx2 = (sidx * 2) + 1;
+			cnt = (cidx * 2) + 1;
+		
+			temp[cnt - 1] = arr[idx1 - 1] * arr[LEN + idx2 - 1];
+			temp[cnt] = arr[idx1] + arr[LEN + idx2];
+			sidx++;
+			cidx++;
+			
+		}
+		fidx++;
+
+	}
+
+	cidx = 0;
+	
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 24 - i; j++)
+		{
+			if (temp[(j*2)+1] < temp[(j * 2) + 3])
+			{
+				tvalue1 = temp[(j * 2)];
+				tvalue2 = temp[(j * 2) + 1];
+
+				temp[(j * 2)] = temp[(j * 2) + 2];
+				temp[(j * 2) + 1] = temp[(j * 2) + 3];
+
+				temp[(j * 2) + 2] = tvalue1;
+				temp[(j * 2) + 3] = tvalue2;
+
+			}
+		}
+	}
+
+	cidx = 0;
+	ridx = 0;
+
+	for (i = 0; i < 24; i++)
+	{
+		cnt = (cidx * 2) + 1;
+		if (temp[cnt] > temp[cnt + 2])
+		{
+			mresult[ridx] = temp[cnt - 1];
+			mresult[ridx + 1] = temp[cnt];
+		}
+		else if (temp[cnt] == temp[cnt + 2])
+		{
+			temp[cnt + 1] = temp[cnt + 1] + temp[cnt - 1];
+		
+		}
+		ridx = ridx + 2;
+		cidx++;
+	}
+
+	mresult[ridx] = temp[cnt - 1];
+	mresult[ridx + 1] = temp[cnt];
+
 
 	for (i = 0; i < 10; i++)
 	{
-		if (result[(i * 2)] == 0 && result[(i * 2) + 1] == 0)
+		if (result[(i * 2)] != 0)
 		{
-			continue;
+			printf("덧셈의결과 ----> 계수:%d 차수:%d   ", result[(i * 2)], result[(i * 2) + 1]);
+			printf("\n");
 		}
-		else
-			printf("계수:%d 차수:%d   ", result[(i * 2)], result[(i * 2) + 1]);
+
 		
-		printf("\n");
+	}
+
+	printf("\n");
+
+	for (i = 0; i < 25; i++)
+	{
+		if (mresult[(i * 2)] != 0)
+		{
+			printf("곱셈의결과 ----> 계수:%d 차수:%d   ", mresult[(i * 2)], mresult[(i * 2) + 1]);
+			printf("\n");
+		}
+
+
 	}
 
 	system("pause");
