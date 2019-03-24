6d000f80cbd09c1ce811936c389a49be00886260
최병준 <qudwns2052@gmail.com>
2019-03-24 15:09:06 +0900

    190324

---
+++ 190324/1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct NODE 
+{
+	struct NODE *next;
+	int data;
+};
+
+void addFirst(struct NODE *target, int data)
+{
+	struct NODE *newNode = malloc(sizeof(struct NODE));
+	newNode->next = target->next;
+	newNode->data = data;
+
+	target->next = newNode;
+}
+
+struct NODE *findNode(struct NODE *node, int data)
+{
+	while (node != NULL)
+	{
+		if (node->data == data)
+		{
+			return node;
+		}
+		else
+		{
+			node = node->next;
+		}
+	}
+	return NULL;
+}
+
+int main()
+{
+	struct NODE *head = malloc(sizeof(struct NODE));
+	head->next = NULL;
+
+	addFirst(head, 10);
+	addFirst(head, 20);
+	addFirst(head, 30);
+
+	struct NODE *found = findNode(head, 20);
+	printf("%d\n", found->data);
+
+	struct NODE *curr = head->next;
+	while (curr != NULL)
+	{
+		struct NODE *next = curr->next;
+		free(curr);
+		curr = next;
+	}
+
+	free(head);
+
+
+	system("pause");
+	return 0;
+}
