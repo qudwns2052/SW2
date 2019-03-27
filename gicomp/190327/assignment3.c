8d195dc475b8129c70c5716630734eff95bbd25d
최병준 <qudwns2052@gmail.com>
2019-03-27 18:27:31 +0900

    190327

---
+++ gicomp/190327/assignment3.c
@@ -0,0 +1,98 @@
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int n1 = 7, n2 = 5;
+//	n1 += n2; // n1 = n1 + n2;
+//	printf("n1 += n2 : %d\n", n1);
+//
+//	n2 += 12; //n2 + 12;
+//	printf("n2 += 12: %d\n", n2);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num1 = 7;
+//	int num2, num3;
+//
+//	num2 == ++num1;
+//	num3 == --num1;
+//
+//	printf("num1 : %d\n", num1);
+//	printf("num2 : %d\n", num2);
+//	printf("num3 : %d\n", num3);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num1 = 7;
+//	int num2, num3;
+//
+//	num2 = num1++;
+//	num3 = num1--;
+//
+//	printf("num1 : %d\n", num1);
+//	printf("num2 : %d\n", num2);
+//	printf("num3 : %d\n", num3);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int num1, num2;
+//	num1 = 7;
+//	num2 = (num1--) + 5;
+//
+//	printf("num1 : %d\n", num1);
+//	printf("num2 : %d\n", num2);
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x = 3;
+//	int y = 2;
+//	printf("%d\n", (x > y) ? x : y);
+//	printf("%d\n", (x < y) ? x : y);
+//
+//	return 0;
+//}
+
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(void)
+//{
+//	int x, y, result;
+//	printf("enter two integers: ");
+//	scanf("%d %d", &x, &y);
+//
+//	result = x + y;
+//	printf("%d + %d = %d\n", x, y, result);
+//
+//	result = x - y;
+//	printf("%d - %d = %d\n", x, y, result);
+//
+//	result = x * y;
+//	printf("%d * %d = %d\n", x, y, result);
+//
+//	result = x / y;
+//	printf("%d / %d = %d\n", x, y, result);
+//
+//	result = x % y;
+//	printf("%d % %d = %d\n", x, y, result);
+//
+//	return 0;
+//}
